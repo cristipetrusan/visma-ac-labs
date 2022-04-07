d2f154c87832c627d95f3607cced68d6ee617c9d
Razvan Smeu <razvan.smeu@visma.com>
2022-04-07 15:11:42 +0300

    Employee API started

---
+++ src/main/java/com/doubletex/app/api/employee/EmployeeAPI.java
@@ -0,0 +1,27 @@
+package com.doubletex.app.api.employee;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/employee")
+@RequiredArgsConstructor
+public class EmployeeAPI {
+    private final EmployeeService employeeService;
+
+    @GetMapping("/{id}")
+    public Optional<Employee> get(@PathVariable Long id) {
+        return employeeService.get(id);
+    }
+
+    @PostMapping("")
+    public void post(@RequestBody Employee employee) {
+        employeeService.post(employee);
+    }
+}
+
+// api/employee?id=1
+// api/employee/1
